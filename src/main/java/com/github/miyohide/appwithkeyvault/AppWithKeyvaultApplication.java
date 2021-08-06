d91f70c018033f16d612bc5d4817dbaf5c25404f
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-06 21:09:05 +0900

    create project using Spring Initializr

---
+++ src/main/java/com/github/miyohide/appwithkeyvault/AppWithKeyvaultApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.appwithkeyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithKeyvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithKeyvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/miyohide/appwithkeyvault/AppWithKeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.appwithkeyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithKeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
